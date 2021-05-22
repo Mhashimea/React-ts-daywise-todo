e6f098815ea75283e41a5171b148a8a1671c5b91
Mhashimea <hashimea12@gmail.com>
2021-05-22 10:02:29 +0530

    added todo card

--- src/components/ux/TodoCard.tsx
+++ src/components/ux/TodoCard.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import { AntDesignOutlined, CalendarOutlined, UserOutlined } from '@ant-design/icons';
+import { Checkbox, Tag, Tooltip, Avatar, Progress } from 'antd';
+import React from 'react';
 
-const TodoCard = () => {
-    return (
-        <div>
-            TodoCard
+export default function TodoCard() {
+  return (
+    <div className="bg-white border  p-4 rounded-md todo-card mb-3">
+      <div className="flex items-start">
+        <Checkbox></Checkbox>
+        <div className="flex items-start ml-2">
+          <div>
+            <h1 className="text-base font-semibold mb-1">
+              Search inspiration for upcoming project
+          </h1>
+            <p className="text-xs text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+              debitis repudiandae explicabo voluptate laborum delectus sapiente
+              nisi soluta doloremque, itaque facilis numquam repellat nobis
+              placeat deleniti ab laboriosam iusto iure!
+          </p>
+          </div>
+          <div className="flex items-center">
+            <Tag className="rounded-md mr-2" color="#1a73e8">
+              Label
+          </Tag>
+            <Tag className="rounded-md" color="red">
+              High Priority
+          </Tag>
+          </div>
         </div>
-    )
+      </div>
+      <div className="mt-3 ml-5 flex items-center justify-between">
+        <Avatar.Group maxCount={2} maxStyle={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+          <Tooltip title="Ant User" placement="top">
+            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+          </Tooltip>
+          <Avatar style={{ backgroundColor: '#1890ff' }} icon={<AntDesignOutlined />} />
+        </Avatar.Group>
+        <span className="flex items-center"><CalendarOutlined className="mr-1" /> 22/05/2021</span>
+        <span className="flex items-center"><UserOutlined className="mr-1" /> Hashim Ea</span>
+        <div className="w-1/5">
+          <Progress percent={30} />
+        </div>
+        <div className="flex items-center">
+          <Avatar>
+            <span>R</span>
+          </Avatar>
+          <h1 className="ml-2 font-medium">Todo App</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default TodoCard
--- src/pages/Todos/index.tsx
+++ src/pages/Todos/index.tsx
@@ -1,31 +1,30 @@
-import { FilterOutlined } from '@ant-design/icons'
-import { Button, Dropdown, Menu } from 'antd'
-import React, { useState } from 'react'
-import DatePicker from '../../components/datepicker'
-import Default from '../../components/layouts/Default'
-import Header from '../../components/ux/Header'
+import { FilterOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Menu } from 'antd';
+import React, { useState } from 'react';
+import DatePicker from '../../components/datepicker';
+import Default from '../../components/layouts/Default';
+import Header from '../../components/ux/Header';
+import TodoCard from '../../components/ux/TodoCard';
 
 export default function Todos() {
-  const [priority, setPriority] = useState<any>("Priority")
-  const [status, setStatus] = useState<any>("All")
+  const [priority, setPriority] = useState<any>('Priority');
+  const [status, setStatus] = useState<any>('All');
 
   const priorityMenu = (
-    <Menu onClick={e => setPriority(e.key)}>
+    <Menu onClick={(e) => setPriority(e.key)}>
       <Menu.Item key="Priority">
         <span>All</span>
       </Menu.Item>
       <Menu.Item key="High">
         <span>High</span>
       </Menu.Item>
-      <Menu.Item key="Medium">
-        Medium
-      </Menu.Item>
+      <Menu.Item key="Medium">Medium</Menu.Item>
       <Menu.Item key="Low">Low</Menu.Item>
     </Menu>
-  )
+  );
 
   const statusMenu = (
-    <Menu onClick={e => setStatus(e.key)}>
+    <Menu onClick={(e) => setStatus(e.key)}>
       <Menu.Item key="All">
         <span>All</span>
       </Menu.Item>
@@ -36,7 +35,7 @@ export default function Todos() {
         <span>Pending</span>
       </Menu.Item>
     </Menu>
-  )
+  );
 
   return (
     <Default className="todos app-container m-auto w-full">
@@ -48,22 +47,38 @@ export default function Todos() {
             <div className="flex items-center">
               <h1 className="text-base font-semibold flex-1">Tasks</h1>
               <div className="flex items-center">
-                <Dropdown overlay={priorityMenu} trigger={['click']} className="mr-2">
+                <Dropdown
+                  overlay={priorityMenu}
+                  trigger={['click']}
+                  className="mr-2"
+                >
                   <Button className="rounded-md flex items-center">
                     <FilterOutlined className="text-sm" />
-                    {priority}</Button>
+                    {priority}
+                  </Button>
                 </Dropdown>
                 <Dropdown overlay={statusMenu} trigger={['click']}>
                   <Button className="rounded-md flex items-center">
                     <FilterOutlined className="text-sm" />
-                    {status}</Button>
+                    {status}
+                  </Button>
                 </Dropdown>
               </div>
             </div>
-            <p className="text-xs text-gray-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, voluptatibus harum? Totam accusamus architecto eveniet doloribus expedita, reprehenderit, optio sit quos quidem hic quasi, explicabo quas aliquam saepe quia itaque?</p>
+            <p className="text-xs text-gray-500 mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+              voluptatibus harum? Totam accusamus architecto eveniet doloribus
+              expedita, reprehenderit, optio sit quos quidem hic quasi,
+              explicabo quas aliquam saepe quia itaque?
+            </p>
+            <div className="mt-5">
+              {[0, 1, 1, 2, 2, 2, 2, 2].map((item) => {
+                return <TodoCard />;
+              })}
+            </div>
           </div>
         </div>
       </div>
     </Default>
-  )
+  );
 }
